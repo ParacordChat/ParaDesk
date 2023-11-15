5dd5d1dc00595748242c4430eaa37152d8f3b126
Jacob Trock <jtbosshku@gmail.com>
2023-11-15 08:55:32 -0500

    remove fe folder

--- frontend/src/helpers/dataHandling/uint8util.ts
+++
@@ -1,6 +0,0 @@
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
-
-export const encodeBytes = (txt: string | undefined) => encoder.encode(txt);
-
-export const decodeBytes = (txt: any | undefined) => decoder.decode(txt);
